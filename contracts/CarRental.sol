19c5083611e8a6bb9ff6390db4c5377c5af92a27
theHardCoder05 <bitlabs27@gmail.com>
2021-11-13 15:46:48 +0900

    add unit test cases

--- contracts/CarRental.sol
+++ contracts/CarRental.sol
@@ -1,10 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.16 <0.9.0;
-import './IRental.sol';
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+contract CarRental {
+    address public owner;
+    // Enum status for car rental
+    enum State {ForRent, Occuppied, UnderMaintenance }
 
-/**
-Car Rental Smart Contract */
-contract CarRental is  Ownable{
+    // Cars records
+    mapping(uint => Car) public Cars;
+
+    // events for car rental
+    event LogForRent(uint id);
+
+    // event log for Occuppied
+    event LogForOccuppied(uint id);
+
+    // event Car rental contract created
+    event LogForCreated(address add);
+
+    // Car Struct
+    struct Car {
+        string name;
+        uint fee;
+        State state;
+        address payable renter;
+        address payable owner;
+    }
+
+    // modifier to check paidEnough
+      modifier paidEnough(uint _fee) { 
+        require(msg.value >= _fee); 
+        _;
+    }
+
+    // public constructor
+    constructor() public  {
+        owner = msg.sender;
+        emit LogForCreated(address(this));
+    }
 
 }
